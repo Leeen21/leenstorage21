8a06fc7d56dc1aa89cf3a25142b33bc4b820bb72
Leeen21 <leenibrahim217@gmail.com>
2025-12-28 17:50:57 +0200

    leen.js

--- leen.js
+++ leen.js
@@ -3,9 +3,9 @@ window.onload = function () {
   // ===== SAVE DATA =====
   localStorage.setItem("Name", "Leen Ibrahim");
   localStorage.setItem("ID", "21");
-  localStorage.setItem("Email", "leen@gmail.com");
-  localStorage.setItem("Phone", "011111111111");
-  localStorage.setItem("Address", "Banha");
+  localStorage.setItem("Email", "leenibrahim217@gmail.com");
+  localStorage.setItem("Phone", "01234567890");
+  localStorage.setItem("Address", "Maadi");
 
   localStorage.setItem(
     "LinkedIn",
@@ -36,4 +36,5 @@ localStorage.setItem("ProfilePic", "leeno/leen.jpeg");
 
   document.getElementById("profilePic").src =
     localStorage.getItem("ProfilePic");
+
 };
